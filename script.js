46a9c0764bbcd0b5e153cd4fbb52df66fff1cbbd
Austine Gomez <austine.gomez@yahoo.com>
2023-06-29 21:53:39 -0500

    modal

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+function openShareModal() {
+    var modal = document.getElementById("shareModal");
+    modal.style.display = "block";
+  }
+  
+  function closeShareModal() {
+    var modal = document.getElementById("shareModal");
+    modal.style.display = "none";
+  }
+  
+  function share() {
+    var shareInput = document.getElementById("shareInput");
+    var content = shareInput.value;
+    
+    // Replace the following code with your actual share functionality
+    // For example, you can use a library or an API to handle the sharing
+    alert("Sharing content: " + content);
+  }
