fc56ad278bab98193117877a9254f910f4105a6f
Austine Gomez <austine.gomez@yahoo.com>
2023-07-03 23:32:44 -0500

    modal css

--- script.js
+++ script.js
@@ -6,18 +6,10 @@ const closeModal = () => {
     toggleModal("none");
 }
 
-const outsideClick = (e) => {
-    if (e.target !== document.getElementById("shareModal")) {
-        console.log("click");
-        closeModal();
-    }
-}
-
 const toggleModal = (displayValue) => {
     const shareModal = document.getElementById("shareModal");
 
     shareModal.style.display = displayValue;
     shareModal.addEventListener("click", openModal);
-    shareModal.addEventListener("click", outsideClick);
     shareModal.addEventListener("click", closeModal);
 }
